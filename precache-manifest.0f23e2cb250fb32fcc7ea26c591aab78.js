117f50f061b1b1fe1f016ff718522f450fe5831f
deepti <kingerdeepti123@gmail.com>
2020-05-01 20:56:29 +0530

    Updates

---
+++ precache-manifest.0f23e2cb250fb32fcc7ea26c591aab78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600b61b65bdd84bcf361e11838246f14",
+    "url": "/burgerBuilder/index.html"
+  },
+  {
+    "revision": "fbda3a23ec27cc5425d3",
+    "url": "/burgerBuilder/static/css/main.f748be5b.chunk.css"
+  },
+  {
+    "revision": "89afa869f18a213c6975",
+    "url": "/burgerBuilder/static/js/2.7facc79f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burgerBuilder/static/js/2.7facc79f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbda3a23ec27cc5425d3",
+    "url": "/burgerBuilder/static/js/main.f0098dcd.chunk.js"
+  },
+  {
+    "revision": "26e497d4921675e38483",
+    "url": "/burgerBuilder/static/js/runtime-main.41256863.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerBuilder/static/media/logo.b8503d26.png"
+  }
+]);
